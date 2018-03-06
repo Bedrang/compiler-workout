21a0532cdbe3b2a8a7dba3991dc2814100215973
Bedrang <Bedrang12@yandex.ru>
2018-03-06 15:08:51 +0000

    Update Language.ml

--- src/Language.ml
+++ src/Language.ml
@@ -2,7 +2,8 @@
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
 open GT
-
+open List
+open Ostap
 (* Opening a library for combinator-based syntax analysis *)
 open Ostap.Combinators
        
@@ -44,8 +45,38 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    
+    let rec eval state expr = 
+    match expr with
+    | Var v -> state v 
+    | Const c -> c 
+    | Binop (operators, expr1, expr2) ->
+    let right = eval state expr1 in
+    let left = eval state expr2 in
+    let numericbool numbers = if numbers != 0 then true else false in 
+    let boolnumeric numbers = if numbers then 1 else 0 in
+    match operators with
+    |"+" -> (right + left)
+    |"-" -> (right - left)
+    |"*" -> (right * left)
+    |"/" -> (right / left)
+    |"%" -> (right mod left)
+    |"<"  -> boolnumeric (right < left)
+    |"<=" -> boolnumeric (right <= left)
+    |">"  -> boolnumeric (right > left)
+    |">=" -> boolnumeric (right >= left)
+    |"==" -> boolnumeric (right == left)
+    |"!=" -> boolnumeric (right != left)
+    |"!!" -> boolnumeric (numericbool right || numericbool left)
+    |"&&" -> boolnumeric (numericbool right && numericbool left)
+    | _ -> failwith "Error"
 
+      let binarylist operatorslist = 
+      let listof operators = (ostap ($(operators)), 
+      fun expr1 expr2 -> Binop (operators, expr1, expr2)) in 
+      List.map listof operatorslist;;
+      
+     
     (* Expression parser. You can use the following terminals:
 
          IDENT   --- a non-empty identifier a-zA-Z[a-zA-Z0-9_]* as a string
@@ -53,7 +84,19 @@ module Expr =
    
     *)
     ostap (
-      parse: empty {failwith "Not implemented yet"}
+      primary: v:IDENT {Var v} | v:DECIMAL {Const v} | -"(" parse -")";
+      parse: 
+        !(Ostap.Util.expr
+          (fun v -> v)
+          [|
+            `Lefta, binarylist ["!!"];
+            `Lefta, binarylist ["&&"];
+            `Nona,  binarylist [">="; ">"; "<="; "<"; "=="; "!="];
+            `Lefta, binarylist ["+"; "-"];
+            `Lefta, binarylist ["*"; "/"; "%"]
+          |]
+          primary
+        )
     )
 
   end
@@ -78,12 +121,23 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+     let rec eval sio stmt =
+      let (statement, input, output) = sio in
+      match stmt with
+	|Read v -> let head = hd input in let tail = tl input in (Expr.update v head statement, tail, output)
+	|Write expr ->  let result = Expr.eval statement expr in (statement, input, output @ [result])
+	|Assign(v, expr) -> let result = Expr.eval statement expr in (Expr.update v result statement, input, output)
+	|Seq(seq1, seq2) -> (eval (eval (statement, input, output) seq1) seq2) 
 
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not implemented yet"}
-    )
+      parse: seq | stmt;
+      stmt: read | write | assign;
+      read: "read" -"(" v:IDENT -")" { Read v };
+      write: "write" -"(" expr:!(Expr.parse) -")" { Write expr };
+      assign: v:IDENT -":=" expr:!(Expr.parse) { Assign (v, expr) };
+      seq: seq1:stmt -";" seq2:parse { Seq (seq1, seq2) }
+     )
       
   end
 
