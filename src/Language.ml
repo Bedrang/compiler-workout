53afaa8eccb602e057f61810dabfeca6b9bcf2d2
Bedrang <Bedrang12@yandex.ru>
2018-04-03 12:49:55 +0100

    Update Language.ml

--- src/Language.ml
+++ src/Language.ml
@@ -45,8 +45,8 @@ module Expr =
  
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
-     *)                                                       
-    let rec eval state expr = 
+    *)                                                       
+        let rec eval state expr = 
     match expr with
     | Var v -> state v 
     | Const c -> c 
@@ -109,8 +109,12 @@ module Stmt =
     (* read into the variable           *) | Read   of string
     (* write the value of an expression *) | Write  of Expr.t
     (* assignment                       *) | Assign of string * Expr.t
-    (* composition                      *) | Seq    of t * t with show
-
+    (* composition                      *) | Seq    of t * t 
+    (* empty statement                  *) | Skip
+    (* conditional                      *) | If     of Expr.t * t * t
+    (* loop with a pre-condition        *) | While  of Expr.t * t
+    (* loop with a post-condition       *) | Repeat of Expr.t * t  with show
+                                                                    
     (* The type of configuration: a state, an input stream, an output stream *)
     type config = Expr.state * int list * int list 
 
@@ -123,20 +127,50 @@ module Stmt =
     let rec eval sio stmt =
       let (statement, input, output) = sio in
       match stmt with
-	|Read v -> let head = hd input in let tail = tl input in (Expr.update v head statement, tail, output)
-	|Write expr ->  let result = Expr.eval statement expr in (statement, input, output @ [result])
-	|Assign(v, expr) -> let result = Expr.eval statement expr in (Expr.update v result statement, input, output)
+	|Read v -> 
+	let head = hd input in 
+	let tail = tl input in (Expr.update v head statement, tail, output)
+	|Write expr ->  
+	let result = Expr.eval statement expr in (statement, input, output @ [result])
+	|Assign(v, expr) -> 
+	let result = Expr.eval statement expr in (Expr.update v result statement, input, output)
 	|Seq(seq1, seq2) -> (eval (eval (statement, input, output) seq1) seq2) 
-                                
-    (* Statement parser *)
+	| Skip -> (statement, input, output)
+        | If (expr, s1, s2) -> if Expr.eval statement expr != 0 then eval (statement, input, output) s1 else eval (statement, input, output) s2
+        | While  (expr, s) -> if Expr.eval statement expr != 0 then eval (eval (statement, input, output) s) stmt else (statement, input, output)
+        | Repeat (expr, s) -> 
+        let (statement', input', output') = eval (statement, input, output) s in
+        if Expr.eval statement' expr == 0 then eval (statement', input', output') stmt else (statement', input', output')
+	
+	let rec elif fi sf = match fi with
+        | [] -> sf
+        | (expr, s)::fi' -> If (expr, s, elif fi' sf)
+	
+	
     ostap (
-      parse: seq | stmt;
-      stmt: read | write | assign;
-      read: "read" -"(" v:IDENT -")" { Read v };
-      write: "write" -"(" expr:!(Expr.parse) -")" { Write expr };
-      assign: v:IDENT -":=" expr:!(Expr.parse) { Assign (v, expr) };
-      seq: seq1:stmt -";" seq2:parse { Seq (seq1, seq2) }
+            parse:
+       st:stmt ";" ps:parse {Seq (st, ps)}
+      | stmt;
+      stmt:
+       "read" "(" v:IDENT ")"             {Read v}
+      | "write" "(" expr:!(Expr.parse) ")" {Write expr}
+      | v:IDENT ":=" expr:!(Expr.parse)    {Assign (v, expr)}            
+      | %"skip" {Skip}
+      | %"while" expr: !(Expr.parse) %"do" s:parse %"od" {While (expr, s)}
+      | %"repeat" s:parse "until" expr:!(Expr.parse) {Repeat (expr, s)}
+      | %"for" init:parse "," expr:!(Expr.parse) "," step:parse %"do" s:parse %"od" {Seq (init, While (expr, Seq (s, step)))}
+      | %"if" expr: !(Expr.parse) 
+        %"then" s:parse 
+            s2:(%"elif" !(Expr.parse) %"then" parse)*
+            sf:(%"else" parse)?
+        %"fi" {If (expr, s, 
+            match sf with 
+              | None -> elif s2 Skip
+              | Some expr -> elif s2 expr 
+            )}
+      
      )
+
       
   end
 
